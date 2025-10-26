c9084bccf08edc6acb5082f8dacc45ec0c9b0816
Jazmín Rayo <jazmin.rayo.caicedo@utelvt.edu.ec>
2025-10-26 03:59:55 -0500

    Tarea subida por Jazmín Rayo en Flutter ibm6b_j_r

--- android/app/src/main/kotlin/com/example/app_apple6b/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.app_apple6b
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ ibm6b_j_r/android/app/src/main/kotlin/com/example/ibm6b_j_r/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ibm6b_j_r
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
